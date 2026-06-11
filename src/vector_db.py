bf9aacfe3d9528826860ebc8acb449a3adf2be27
Aimalll-2004 <aimaljaved.0@gmail.com>
2026-06-11 13:24:35 +0200

    minor changes

--- src/vector_db.py
+++ src/vector_db.py
@@ -2,6 +2,7 @@ from langchain_community.document_loaders import DirectoryLoader, TextLoader, Py
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from pathlib import Path
 
 all_docs = []
 
